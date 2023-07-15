f51ef66657a2a3af090900281c86da61c53d5cb0
kimseulgi <rlatmfrl9@naver.com>
2023-07-15 22:49:21 +0900

    Design: main페이지 html/css 작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Router from './shared/Router';
+import './style/reset.css';
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@700&display=swap');
+</style>;
 
 function App() {
   const queryClient = new QueryClient();
