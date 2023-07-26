839bf68bcd01b6396cd44041022ba0e2d15f5f55
kimseulgi <rlatmfrl9@naver.com>
2023-07-27 00:51:47 +0900

    Design: 멸종위기 동물 카드 추가, hover 효과 적용

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import Footer from './Footer';
 import { StPTitle, StSection } from './Main';
-import animal from '../images/animals/animal1.png';
+import animal1 from '../images/animals/animal1.png';
+import animal2 from '../images/animals/animal2.png';
+import animal3 from '../images/animals/animal3.png';
+import animal4 from '../images/animals/animal4.png';
+import animal5 from '../images/animals/animal5.png';
+import animal6 from '../images/animals/animal6.png';
+import animal7 from '../images/animals/animal7.png';
+import animal8 from '../images/animals/animal8.png';
+import animal9 from '../images/animals/animal9.png';
+import animal10 from '../images/animals/animal10.png';
 import { styled } from 'styled-components';
 
 function Animal() {
@@ -13,7 +22,61 @@ function Animal() {
           <StAnimalName>
             <p>갈라파고스 물개</p>
           </StAnimalName>
-          <img src={animal} />
+          <img src={animal1} />
+        </StAnimalCard>
+        <StAnimalCard>
+          <StAnimalName>
+            <p>돌묵상어</p>
+          </StAnimalName>
+          <img src={animal2} />
+        </StAnimalCard>
+        <StAnimalCard>
+          <StAnimalName>
+            <p>북극곰</p>
+          </StAnimalName>
+          <img src={animal3} />
+        </StAnimalCard>
+        <StAnimalCard>
+          <StAnimalName>
+            <p>산호상어</p>
+          </StAnimalName>
+          <img src={animal4} />
+        </StAnimalCard>
+        <StAnimalCard>
+          <StAnimalName>
+            <p>수마트라 수달</p>
+          </StAnimalName>
+          <img src={animal5} />
+        </StAnimalCard>
+        <StAnimalCard>
+          <StAnimalName>
+            <p>아마존강 돌고래</p>
+          </StAnimalName>
+          <img src={animal6} />
+        </StAnimalCard>
+        <StAnimalCard>
+          <StAnimalName>
+            <p>저어새</p>
+          </StAnimalName>
+          <img src={animal7} />
+        </StAnimalCard>
+        <StAnimalCard>
+          <StAnimalName>
+            <p>지중해 몽크물범</p>
+          </StAnimalName>
+          <img src={animal8} />
+        </StAnimalCard>
+        <StAnimalCard>
+          <StAnimalName>
+            <p>페어리 펭귄</p>
+          </StAnimalName>
+          <img src={animal9} />
+        </StAnimalCard>
+        <StAnimalCard>
+          <StAnimalName>
+            <p>푸른바다 거북</p>
+          </StAnimalName>
+          <img src={animal10} />
         </StAnimalCard>
       </StAnimalCardWrap>
       <Footer />
@@ -24,6 +87,9 @@ function Animal() {
 export default Animal;
 const StAnimalCardWrap = styled.div`
   margin: 60px 0;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 50px;
 `;
 const StAnimalCard = styled.div`
   width: 216px;
@@ -42,4 +108,15 @@ const StAnimalName = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  opacity: 0;
+  -webkit-backdrop-filter: blur(2px);
+  -mos-backdrop-filter: blur(2px);
+  -ms-backdrop-filter: blur(2px);
+  -o-backdrop-filter: blur(2px);
+  backdrop-filter: blur(2px);
+  background-color: rgba(0, 0, 0, 0.5);
+  transition: all 0.8s;
+  &:hover {
+    opacity: 100%;
+  }
 `;
